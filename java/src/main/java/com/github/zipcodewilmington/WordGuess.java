f749b7649c79b93809fcce9d222f3de6f7fae4a1
gitanitraj <anitra.com@gmail.com>
2026-04-13 21:25:20 -0400

    I added basic loop

--- java/src/main/java/com/github/zipcodewilmington/WordGuess.java
+++ java/src/main/java/com/github/zipcodewilmington/WordGuess.java
@@ -5,4 +5,19 @@ package com.github.zipcodewilmington;
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
-public class WordGuess {}
+public class WordGuess {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Word Guess!");
+    
+    while (true) {
+        System.out.print("Guess a letter: ");
+        String letterToGuess = scanner.nextLine();
+        
+        if (userGuess.equalsIgnoreCase(letterToGuess)) {
+            System.out.println("Congratulations! You've guessed the word!");
+            break;
+        } else {
+            System.out.println("Wrong guess. Try again!");
+        }
+}
