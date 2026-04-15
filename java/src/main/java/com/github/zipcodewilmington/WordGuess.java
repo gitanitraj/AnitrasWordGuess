3baa1d51b1a62f5b9ff4a26758262f60c8ed1c42
gitanitraj <anitra.com@gmail.com>
2026-04-14 21:24:50 -0400

    Person Class removed from WordGuess file

--- java/src/main/java/com/github/zipcodewilmington/WordGuess.java
+++ java/src/main/java/com/github/zipcodewilmington/WordGuess.java
@@ -104,22 +104,3 @@ public class WordGuess {
         }
     }
 }   
-
-public class Person {
- 
-    private String name;
-    private int score;
-    private int gamesPlayed;
-    private int gamesWon;
- 
-    public Person(String name) {
-        this.name = name;
-        this.score = 0;
-    }
-
-    public void updateScore(int points) {
-        this.score += points;
-    }
-
-
-}
