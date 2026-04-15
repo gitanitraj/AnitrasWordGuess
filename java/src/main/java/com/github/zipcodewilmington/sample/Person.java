8affaf4379f56205ea7ee03a09ce765eedc137fa
gitanitraj <anitra.com@gmail.com>
2026-04-14 21:26:47 -0400

    Person Class added to Person file

--- java/src/main/java/com/github/zipcodewilmington/sample/Person.java
+++ java/src/main/java/com/github/zipcodewilmington/sample/Person.java
@@ -1,4 +1,20 @@
 package com.github.zipcodewilmington.sample;
 
 public class Person {
+ 
+    private String name;
+    private int score;
+    private int gamesPlayed;
+    private int gamesWon;
+ 
+    public Person(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public void updateScore(int points) {
+        this.score += points;
+    }
+
+
 }
